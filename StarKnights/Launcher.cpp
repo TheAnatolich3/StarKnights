4b63f55c9c0ce84b2125c79fb8f1451757d91154
TheAnatolich <zakrevskii3112@gmail.com>
2022-01-08 16:48:55 +0300

    Добавьте файлы проекта.

---
+++ StarKnights/Launcher.cpp
@@ -0,0 +1,11 @@
+﻿// StarKnights.cpp: определяет точку входа для приложения.
+//
+#include <iostream>
+#include <string_view>
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	std::string_view str = "srgfsr";
+	return 0;
+}
