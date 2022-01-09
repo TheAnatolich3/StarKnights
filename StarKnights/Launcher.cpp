1c6b157cd057e42cc252ee1921f750ef19fe73a3
TheAnatolich <zakrevskii3112@gmail.com>
2022-01-09 09:19:26 +0300

    Add libraries: SDL2, imgui, glew, glm, stbi

--- StarKnights/Launcher.cpp
+++ StarKnights/Launcher.cpp
@@ -2,10 +2,15 @@
 //
 #include <iostream>
 #include <string_view>
+#include <Engine.hpp>
+#include <glm/glm.hpp>
+#include <SDL.h>
+#include <stbi/stb_image.h>
 
-int main()
+int main(int argc, char* argv[])
 {
 	std::cout << "Hello CMake." << std::endl;
 	std::string_view str = "srgfsr";
+	glm::vec2 size(1.0f, 1.0f);
 	return 0;
 }
---
+++ StarKnights/StarEngine/src/Engine.hpp
@@ -0,0 +1,11 @@
+#ifndef ENGINE_HPP
+#define ENGINE_HPP
+
+class Engine
+{
+public:
+	int test;
+};
+#endif ENGINE_HPP
+
+
